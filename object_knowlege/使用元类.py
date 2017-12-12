57452803474e4ebd20927bfd3867e645bdfb9e8c
wulei <wulei_clec@chinalife.com.cn>
2017-12-12 09:40:41 +0800

    add some code

---
+++ object_knowlege/使用元类.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+
+"""
+
+@author:chinacg
+
+@file:使用元类.py
+
+@time:2017-12-1115:13
+
+"""
+
+
+# type()函数既可以返回一个对象的类型，又可以创建出新的类型，
+# 比如，我们可以通过type()函数创建出Hello类，
+# 而无需通过class Hello(object)...的定义：
+
+
+def fn(self, name='world'):
+    print('Hello, %s.' % name)
+
+
+Hello = type('Hello', (object,), dict(hello=fn))  # 创建Hello class
+
+h = Hello()
+h.hello()
+
+# metaclass
+
+'''除了使用type()动态创建类以外，要控制类的创建行为，还可以使用metaclass。
+
+metaclass，直译为元类，简单的解释就是：
+
+当我们定义了类以后，就可以根据这个类创建出实例，所以：先定义类，然后创建实例。
+
+但是如果我们想创建出类呢？那就必须根据metaclass创建出类，所以：先定义metaclass，然后创建类。
+
+连接起来就是：先定义metaclass，就可以创建类，最后创建实例。
+
+所以，metaclass允许你创建类或者修改类。换句话说，你可以把类看成是metaclass创建出来的“实例”。
+
+metaclass是Python面向对象里最难理解，也是最难使用的魔术代码。正常情况下，你不会碰到需要使用metaclass的情况，
+所以，以下内容看不懂也没关系，因为基本上你不会用到。
+
+我们先看一个简单的例子，这个metaclass可以给我们自定义的MyList增加一个add方法：
+
+定义ListMetaclass，按照默认习惯，metaclass的类名总是以Metaclass结尾，以便清楚地表示这是一个metaclass：'''
+
+
--- object_knowlege/枚举类.py
+++ object_knowlege/枚举类.py
@@ -10,3 +10,41 @@
 
 """
 
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+
+# 如果需要更精确地控制枚举类型，可以从Enum派生出自定义类：
+
+from enum import Enum, unique
+
+
+@unique
+class WeekDay(Enum):
+    Sun = 0  # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+
+print(WeekDay.Sun, WeekDay['Sun'], WeekDay.Thu.value)
+
+
+# 练习 把Student的gender属性改造为枚举类型，可以避免使用字符串：
+
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+
+@unique
+class Gender(Enum):
+    Male = 0
+    Female = 1
